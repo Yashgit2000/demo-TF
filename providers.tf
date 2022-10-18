e40a520f5815ff817f82d8648e9ee3c7859000e2
Yash Vardhan Gupta <ya.gupta3@gemsol.in>
2022-10-18 12:57:12 +0530

    First Commit

---
+++ providers.tf
@@ -0,0 +1,22 @@
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=3.0.0"
+#     }
+#   }
+# }
+
+# provider "azurerm" {
+#   features {}
+
+#   subscription_id = "822f8ad2-f586-465a-a53a-bc6719073b4f"
+#   client_id       = "e1df434c-06f0-4585-a912-d3f5a7640a79"
+#   client_secret   = "EfO8Q~YZS3Qn5uLHMtVc2AY8mQ32ubCHXcmKTcA1"
+#   tenant_id       = "254726f2-f5b9-4742-9626-4408cb6f6641"
+# }
+
+# # resource "azurerm_resource_group" "example" {
+# #   name     = "RG1"
+# #   location = var.azure_location
+# # }
